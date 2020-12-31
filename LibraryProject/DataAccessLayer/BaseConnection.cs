32e8ce98e18d315d2f3ab7cbf6c4171eabc630b7
Stefan Cirkovic <stefan.cirkovic2@gmail.com>
2020-12-31 11:32:21 +0100

    Kreirana klasa BaseConnection sa potrebnim metodama

---
+++ LibraryProject/DataAccessLayer/BaseConnection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DataAccessLayer.Constant;
+
+namespace DataAccessLayer
+{
+   public  class BaseConnection
+    {
+        private static SqlCommand GetSqlCommand(string query)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(ConnectionBase.ConnectionString);
+                sqlConnection.Open();
+                return new SqlCommand(query, sqlConnection);
+            }
+            catch
+            {
+                return new SqlCommand();
+            }
+        }
+        public static bool ExecuteNonQuerySqlCommand(string query)
+        {
+            try
+            {
+                SqlCommand sqlCommand = GetSqlCommand(query);
+                return sqlCommand.ExecuteNonQuery() > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static object ExecuteScalarSqlCommand(string query)
+        {
+            try
+            {
+                SqlCommand sqlCommand = GetSqlCommand(query);
+                return sqlCommand.ExecuteScalar();
+            }
+            catch
+            {
+                return new object();
+            }
+        }
+
+    }
+}
---
+++ LibraryProject/DataAccessLayer/Constant/ConnectionBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Constant
+{
+   public  class ConnectionBase
+    {
+        public static string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BaseLibrary;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
